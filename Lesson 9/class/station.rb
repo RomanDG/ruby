79df87d54b0fb2795199e96443ecaedb6acb17c9
RomanDG <romand85@yandex.ru>
2018-01-10 16:38:30 +0300

    Lesson 9

---
+++ Lesson 9/class/station.rb
@@ -0,0 +1,54 @@
+class Station
+  @@stations = []
+
+  def self.all
+    @@stations
+  end
+
+  # здесь я оставил все в public так как используется обьектами из вне
+  attr_accessor :trains, :name
+
+  NAME_FORMAT = /^[а-яА-ЯёЁ\s]+$/
+
+  def initialize(name)
+    @name = name
+    @trains = []
+    validate!
+    @@stations << self
+  end
+
+  # метод, который принимает блок и проходит по всем поездам на станции
+  def show_trains
+    trains.each do |train|
+      yield(train)
+    end
+  end
+
+  def valid?
+    validate!
+    true
+  rescue StandardError
+    false
+  end
+
+  def add_train(train)
+    trains.push(train)
+  end
+
+  def delete_train(train)
+    trains.delete_if do |item|
+      item.type == train.type && item.number == train.number
+    end
+  end
+
+  def get_trains_by_type(type)
+    trains.select { |train| train.type == type }
+  end
+
+  protected
+
+  def validate!
+    raise 'название станции содержит латинские буквы' if name !~ NAME_FORMAT
+    raise 'название станции не должно иметь разделяющих пробелов' if name.split.size > 2
+  end
+end
